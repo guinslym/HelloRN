e9effb94a99ccf3dbbc562cd060809d72edffd93
Nora Mondesir <guinslym@gmail.com>
2017-12-30 23:04:37 -0500

    first app with React-native ios

---
+++ index.js
@@ -0,0 +1,50 @@
+import { AppRegistry } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class HelloRN extends Component<{}> {
+  render() {
+    console.log('Hello world in the console');
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello Guinsly Mondésir!!!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+AppRegistry.registerComponent('HelloRN', () => HelloRN);
